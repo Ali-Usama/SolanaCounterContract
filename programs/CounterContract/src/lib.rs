aabb2c636dbe31b81dd2f8b25cd73fe8a3d86601
Usama Ali <aliusama481@yahoo.com>
2022-09-20 00:25:41 +0500

    Deployed Contract

--- programs/CounterContract/src/lib.rs
+++ programs/CounterContract/src/lib.rs
@@ -1,15 +1,55 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("9Yt8KUyETrZDJm3jg5pt8H2xTD49m8XPitY6cUGSjiwz");
 
 #[program]
 pub mod counter_contract {
     use super::*;
 
+    // handler function to initialize the Counter account
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter_account = &mut ctx.accounts.counter_account;
+        counter_account.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter_account = &mut ctx.accounts.counter_account;
+        counter_account.count += 1;
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<Decrement>) -> Result<()> {
+        let counter_account = &mut ctx.accounts.counter_account;
+        counter_account.count -= 1;
         Ok(())
     }
 }
 
+// transaction instruction to initialize the Counter account
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer=user, space=8+16)]
+    pub counter_account: Account<'info, CounterAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter_account: Account<'info, CounterAccount>
+}
+
+#[derive(Accounts)]
+pub struct Decrement<'info> {
+    #[account(mut)]
+    pub counter_account: Account<'info, CounterAccount>
+}
+
+// this account is used to store the state of the counter
+#[account]
+pub struct CounterAccount {
+    pub count: u64
+}
--- tests/CounterContract.ts
+++ tests/CounterContract.ts
@@ -1,16 +1,48 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { CounterContract } from "../target/types/counter_contract";
+import {Program} from "@project-serum/anchor";
+import {CounterContract} from "../target/types/counter_contract";
+import {assert} from "chai";
 
 describe("CounterContract", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    // Configure the client to use the local cluster.
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
 
-  const program = anchor.workspace.CounterContract as Program<CounterContract>;
+    const program = anchor.workspace.CounterContract as Program<CounterContract>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    const counterAccount = anchor.web3.Keypair.generate();
+    it("Is initialized!", async () => {
+        // Add your test here.
+
+        // signers adds the provider.wallet as a default signer in the array
+        const tx = await program.methods.initialize().accounts({
+            counterAccount: counterAccount.publicKey,
+            user: provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId
+        }).signers([counterAccount]).rpc();
+
+        const createdCounter = await program.account.counterAccount.fetch(counterAccount.publicKey);
+        assert.strictEqual(createdCounter.count.toNumber(), 0)
+        console.log("Your transaction signature", tx);
+    });
+
+    it("Increment the counter!", async () => {
+
+      const tx = await program.methods.increment().accounts({
+        counterAccount: counterAccount.publicKey
+      }).rpc();
+
+      const incrementedCounter = await program.account.counterAccount.fetch(counterAccount.publicKey);
+      assert.strictEqual(incrementedCounter.count.toNumber(), 1);
+    });
+
+    it("Decrement the counter!", async () => {
+
+      const tx = await program.methods.decrement().accounts({
+        counterAccount: counterAccount.publicKey
+      }).rpc();
+
+      const decrementedCounter = await program.account.counterAccount.fetch(counterAccount.publicKey);
+      assert.strictEqual(decrementedCounter.count.toNumber(), 0);
+    })
 });
